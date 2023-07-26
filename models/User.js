0289e1caf56794bdc6a202195929a02ab1de5b74
Tu Nombre <agustinrack14@hotmail.com>
2023-07-26 15:48:15 -0300

    creando las validaciones

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../db");
+const bcrypt = require("bcrypt");
 
 class User extends sequelize.Model {}
 
@@ -26,5 +27,22 @@ User.init(
   },
   { sequelize: db, modelName: "user" }
 );
+
+User.beforeCreate((user) => {
+  const salt = bcrypt.genSaltSync(10);
+  user.salt = salt;
+
+  return user.hash(user.password, user.salt).then((hash) => {
+    user.password = hash;
+  });
+});
+
+Users.prototype.hash = function (plainPassword, salt) {
+  return bcrypt.hash(plainPassword, salt);
+};
+
+Users.prototype.validatePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 module.exports = User;
